26fe1970defeca6d5b096a692f8961d0645b6d0f
sabibi_k <sabina7350@gmail.com>
2024-01-15 23:25:39 +0300

    InitCom

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 7800;
+        int mile = 20;
+        int bonus = ticket / mile;
+
+        System.out.println(bonus);
+    }
+}
